3faf7961d8ebcb1f166affb21f9764703b7c0855
Rashmi Das <rashmidas333@outlook.com>
2026-03-03 10:04:15 +0100

    changes on server.js to deploy on render

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,8 +1,7 @@
 import app from "../app.js";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Start the server
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
